2578920b895091079e8242212cb8e8e4f2dafe1c
misat1505 <misat2002@gmail.com>
2026-08-04 13:37:00 +0200

    feat: added few built-in functions

--- src/std_functions.rs
+++ src/std_functions.rs
@@ -1,9 +1,11 @@
 use std::{
     cell::RefCell,
     collections::HashMap,
-    fs,
+    fs::{self, OpenOptions},
     io::{self, Write},
+    path::Path,
     rc::Rc,
+    writeln,
 };
 
 use crate::{
@@ -213,6 +215,114 @@ impl StdFunction {
         };
         StdFunction { params, execute }
     }
+
+    fn append_file() -> Self {
+        let params = vec![Type::Str, Type::Str];
+        let execute = |params: &Vec<Rc<RefCell<Value>>>| -> Result<Option<Value>, StdFunctionError> {
+            if let Some(filepath) = params.get(0) {
+                if let Some(content) = params.get(1) {
+                    let filepath = filepath.borrow();
+                    let content = content.borrow();
+                    match &*filepath {
+                        Value::String(path) => match &*content {
+                            Value::String(con) => match OpenOptions::new().append(true).create(true).open(path) {
+                                Ok(mut file) => match write!(file, "{}", con) {
+                                    Ok(_) => Ok(None),
+                                    Err(_) => Err(StdFunctionError::new(ErrorSeverity::HIGH, String::from("Failed to append to file."))),
+                                },
+                                Err(_) => Err(StdFunctionError::new(ErrorSeverity::HIGH, String::from("Failed to append to file."))),
+                            },
+                            _ => Err(StdFunctionError::new(
+                                ErrorSeverity::HIGH,
+                                format!(
+                                    "Std function 'write_file' expected '{:?}' as the only argument, but was given '{:?}'.",
+                                    Type::Str,
+                                    filepath.to_type()
+                                ),
+                            )),
+                        },
+                        _ => Err(StdFunctionError::new(
+                            ErrorSeverity::HIGH,
+                            format!(
+                                "Std function 'write_file' expected '{:?}' as the only argument, but was given '{:?}'.",
+                                Type::Str,
+                                filepath.to_type()
+                            ),
+                        )),
+                    }
+                } else {
+                    Err(StdFunctionError::new(
+                        ErrorSeverity::HIGH,
+                        String::from("Missing argument for 'write_file' function."),
+                    ))
+                }
+            } else {
+                Err(StdFunctionError::new(
+                    ErrorSeverity::HIGH,
+                    String::from("Missing argument for 'write_file' function."),
+                ))
+            }
+        };
+        StdFunction { params, execute }
+    }
+
+    fn delete_file() -> Self {
+        let params = vec![Type::Str];
+        let execute = |params: &Vec<Rc<RefCell<Value>>>| -> Result<Option<Value>, StdFunctionError> {
+            if let Some(filepath) = params.get(0) {
+                let filepath = filepath.borrow();
+                match &*filepath {
+                    Value::String(path) => match fs::remove_file(path) {
+                        Ok(_) => Ok(None),
+                        Err(_) => Err(StdFunctionError::new(ErrorSeverity::HIGH, String::from("Failed to delete file."))),
+                    },
+                    _ => Err(StdFunctionError::new(
+                        ErrorSeverity::HIGH,
+                        format!(
+                            "Std function 'delete_file' expected '{:?}' as the only argument, but was given '{:?}'.",
+                            Type::Str,
+                            filepath.to_type()
+                        ),
+                    )),
+                }
+            } else {
+                Err(StdFunctionError::new(
+                    ErrorSeverity::HIGH,
+                    String::from("Missing argument for 'delete_file' function."),
+                ))
+            }
+        };
+        StdFunction { params, execute }
+    }
+
+    fn exists_file() -> Self {
+        let params = vec![Type::Str];
+        let execute = |params: &Vec<Rc<RefCell<Value>>>| -> Result<Option<Value>, StdFunctionError> {
+            if let Some(filepath) = params.get(0) {
+                let filepath = filepath.borrow();
+                match &*filepath {
+                    Value::String(path) => {
+                        let exists = Path::new(path).exists();
+                        return Ok(Some(Value::Bool(exists)));
+                    }
+                    _ => Err(StdFunctionError::new(
+                        ErrorSeverity::HIGH,
+                        format!(
+                            "Std function 'exists_file' expected '{:?}' as the only argument, but was given '{:?}'.",
+                            Type::Str,
+                            filepath.to_type()
+                        ),
+                    )),
+                }
+            } else {
+                Err(StdFunctionError::new(
+                    ErrorSeverity::HIGH,
+                    String::from("Missing argument for 'exists_file' function."),
+                ))
+            }
+        };
+        StdFunction { params, execute }
+    }
 }
 
 pub fn get_std_functions() -> HashMap<String, StdFunction> {
@@ -223,5 +333,8 @@ pub fn get_std_functions() -> HashMap<String, StdFunction> {
     std_functions.insert("mod".to_owned(), StdFunction::modulo());
     std_functions.insert("read_file".to_owned(), StdFunction::read_file());
     std_functions.insert("write_file".to_owned(), StdFunction::write_file());
+    std_functions.insert("append_file".to_owned(), StdFunction::append_file());
+    std_functions.insert("delete_file".to_owned(), StdFunction::delete_file());
+    std_functions.insert("exists_file".to_owned(), StdFunction::exists_file());
     std_functions
 }

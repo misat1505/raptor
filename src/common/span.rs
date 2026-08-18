d18756d74b8951dac9f9267ef6130079a06d8229
misat1505 <misat2002@gmail.com>
2026-08-18 12:42:58 +0200

    feat: put span into errors instead of position, fixed lexer errors

---
+++ src/common/span.rs
@@ -0,0 +1,21 @@
+use crate::common::position::Position;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Span {
+    start: Position,
+    end: Position,
+}
+
+impl Span {
+    pub fn new(start: Position, end: Position) -> Span {
+        Span { start, end }
+    }
+
+    pub fn start(&self) -> Position {
+        self.start
+    }
+
+    pub fn end(&self) -> Position {
+        self.end
+    }
+}

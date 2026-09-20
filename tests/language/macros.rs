f80a4d8ef186588ca4aff37508f14c4c8381eb58
misat1505 <misat2002@gmail.com>
2026-09-20 12:34:27 +0200

    test: updated tests for macros to cover vectors

--- tests/language/macros.rs
+++ tests/language/macros.rs
@@ -27,51 +27,65 @@ enum Role derives Debug {
 struct User derives Debug {
     str name,
     AccountStatus status,
-    Role role
+    Role role,
+    i64[] scores
 };
 
-fn pretty_timestamp(&Timestamp timestamp): str {
-    let day = timestamp.day as str;
-    if (timestamp.day < 10) day = "0" + timestamp.day as str;
-
-    let month = timestamp.month as str;
-    if (timestamp.month < 10) month = "0" + timestamp.month as str;
-
-    return day as str + "-" + month as str + "-" + timestamp.year as str;
-}
+struct UserRepository derives Debug {
+    User[] users
+};
 
 let users = [
     User {
         name: "Alice",
         status: AccountStatus::Active,
-        role: Role::Admin
+        role: Role::Admin,
+        scores: [10, 20, 30]
     },
     User {
         name: "Bob",
         status: AccountStatus::Suspended("too many failed logins"),
-        role: Role::User
+        role: Role::User,
+        scores: [40, 50]
     },
     User {
         name: "Charlie",
-        status: AccountStatus::Deleted(Timestamp { day: 17, month: 9, year: 2026 }),
-        role: Role::Moderator
+        status: AccountStatus::Deleted(Timestamp {
+            day: 17,
+            month: 9,
+            year: 2026
+        }),
+        role: Role::Moderator,
+        scores: [60, 70, 80, 90]
     }
 ];
 
-let idx = 2;
-match (users[idx].status) {
-    AccountStatus::Deleted(timestamp) { println(pretty_timestamp(&timestamp)); },
-    rest println(account_status_debug(&users[idx].status) + " doesn't contain data of type 'Timestamp'.");
+let user_repository = UserRepository { users };
+
+match (user_repository.users[2].status) {
+    AccountStatus::Deleted(timestamp) {
+        println(
+            timestamp.day as str
+            + "-"
+            + timestamp.month as str
+            + "-"
+            + timestamp.year as str
+        );
+    },
+    rest {}
 }
 
-for (let i = 0; i < vector_size(&users); i += 1) {
-    println(user_debug(&users[i]));
+for (let i = 0; i < vector_size(&user_repository.users); i += 1) {
+    println(user_debug(&user_repository.users[i]));
 }
         "#
         .as_bytes(),
     );
 
-    assert_same_output(text, "17-09-2026\nUser { name: \"Alice\", status: AccountStatus::Active, role: Role::Admin }\nUser { name: \"Bob\", status: AccountStatus::Suspended(\"too many failed logins\"), role: Role::User }\nUser { name: \"Charlie\", status: AccountStatus::Deleted(Timestamp { day: 17, month: 9, year: 2026 }), role: Role::Moderator }\n");
+    assert_same_output(
+        text,
+        "17-9-2026\nUser { name: \"Alice\", status: AccountStatus::Active, role: Role::Admin, scores: [10, 20, 30] }\nUser { name: \"Bob\", status: AccountStatus::Suspended(\"too many failed logins\"), role: Role::User, scores: [40, 50] }\nUser { name: \"Charlie\", status: AccountStatus::Deleted(Timestamp { day: 17, month: 9, year: 2026 }), role: Role::Moderator, scores: [60, 70, 80, 90] }\n",
+    );
 }
 
 #[test]
@@ -99,7 +113,8 @@ enum Role derives Json {
 struct User derives Json {
     str name,
     AccountStatus status,
-    Role role
+    Role role,
+    i64[] scores
 };
 
 struct UserRepository derives Json {
@@ -110,20 +125,26 @@ let users = [
     User {
         name: "Alice",
         status: AccountStatus::Active,
-        role: Role::Admin
+        role: Role::Admin,
+        scores: [10, 20, 30]
     },
     User {
         name: "Bob",
         status: AccountStatus::Suspended("too many failed logins"),
-        role: Role::User
+        role: Role::User,
+        scores: [40, 50]
     },
     User {
         name: "Charlie",
-        status: AccountStatus::Deleted(Timestamp { day: 17, month: 9, year: 2026 }),
-        role: Role::Moderator
+        status: AccountStatus::Deleted(Timestamp {
+            day: 17,
+            month: 9,
+            year: 2026
+        }),
+        role: Role::Moderator,
+        scores: [60, 70, 80, 90]
     }
 ];
-
 
 let user_repository = UserRepository { users };
 
@@ -132,7 +153,10 @@ println(user_repository_json_encode(&user_repository));
         .as_bytes(),
     );
 
-    assert_same_output(text, "{\"users\":[{\"name\":\"Alice\",\"status\":\"Active\",\"role\":\"Admin\"},{\"name\":\"Bob\",\"status\":{\"Suspended\":\"too many failed logins\"},\"role\":\"User\"},{\"name\":\"Charlie\",\"status\":{\"Deleted\":{\"day\":17,\"month\":9,\"year\":2026}},\"role\":\"Moderator\"}]}\n");
+    assert_same_output(
+        text,
+        "{\"users\":[{\"name\":\"Alice\",\"status\":\"Active\",\"role\":\"Admin\",\"scores\":[10,20,30]},{\"name\":\"Bob\",\"status\":{\"Suspended\":\"too many failed logins\"},\"role\":\"User\",\"scores\":[40,50]},{\"name\":\"Charlie\",\"status\":{\"Deleted\":{\"day\":17,\"month\":9,\"year\":2026}},\"role\":\"Moderator\",\"scores\":[60,70,80,90]}]}\n",
+    );
 }
 
 #[test]
@@ -160,40 +184,63 @@ enum Role derives Debug, Clone {
 struct User derives Debug, Clone {
     str name,
     AccountStatus status,
-    Role role
+    Role role,
+    i64[] scores
 };
 
-let original = User {
-    name: "Alice",
-    status: AccountStatus::Deleted(Timestamp {
-        day: 17,
-        month: 9,
-        year: 2026
-    }),
-    role: Role::Moderator
+struct UserRepository derives Debug, Clone {
+    User[] users
 };
 
-let cloned = user_clone(&original);
+let original = UserRepository {
+    users: [
+        User {
+            name: "Alice",
+            status: AccountStatus::Active,
+            role: Role::Admin,
+            scores: [10, 20, 30]
+        },
+        User {
+            name: "Bob",
+            status: AccountStatus::Suspended("too many failed logins"),
+            role: Role::User,
+            scores: [40, 50]
+        },
+        User {
+            name: "Charlie",
+            status: AccountStatus::Deleted(Timestamp {
+                day: 17,
+                month: 9,
+                year: 2026
+            }),
+            role: Role::Moderator,
+            scores: [60, 70, 80, 90]
+        }
+    ]
+};
 
-original.name = "Changed";
-match (original.status) {
+let cloned = user_repository_clone(&original);
+
+original.users[0].name = "Changed";
+original.users[0].scores[0] = 999;
+
+match (original.users[2].status) {
     AccountStatus::Deleted(timestamp) {
         timestamp.day = 1;
     },
     rest {}
 }
-original.role = Role::Admin;
 
-println(user_debug(&original));
+original.users[2].role = Role::Admin;
 
-println(user_debug(&cloned));
+println(user_repository_debug(&original));
+println(user_repository_debug(&cloned));
         "#
         .as_bytes(),
     );
 
     assert_same_output(
         text,
-        "User { name: \"Changed\", status: AccountStatus::Deleted(Timestamp { day: 1, month: 9, year: 2026 }), role: Role::Admin }
-User { name: \"Alice\", status: AccountStatus::Deleted(Timestamp { day: 17, month: 9, year: 2026 }), role: Role::Moderator }\n",
+        "UserRepository { users: [User { name: \"Changed\", status: AccountStatus::Active, role: Role::Admin, scores: [999, 20, 30] }, User { name: \"Bob\", status: AccountStatus::Suspended(\"too many failed logins\"), role: Role::User, scores: [40, 50] }, User { name: \"Charlie\", status: AccountStatus::Deleted(Timestamp { day: 1, month: 9, year: 2026 }), role: Role::Admin, scores: [60, 70, 80, 90] }] }\nUserRepository { users: [User { name: \"Alice\", status: AccountStatus::Active, role: Role::Admin, scores: [10, 20, 30] }, User { name: \"Bob\", status: AccountStatus::Suspended(\"too many failed logins\"), role: Role::User, scores: [40, 50] }, User { name: \"Charlie\", status: AccountStatus::Deleted(Timestamp { day: 17, month: 9, year: 2026 }), role: Role::Moderator, scores: [60, 70, 80, 90] }] }\n",
     );
 }

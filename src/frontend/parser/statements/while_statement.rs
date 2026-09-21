f303e9e2e2bca5ccde9500bae50725343dbac11c
misat1505 <misat2002@gmail.com>
2026-09-21 12:41:56 +0200

    feat: implemented while-not-matches syntactic sugar

--- src/frontend/parser/statements/while_statement.rs
+++ src/frontend/parser/statements/while_statement.rs
@@ -1,16 +1,31 @@
 use crate::{
-    common::{errors::IError, span::Span},
+    common::{
+        errors::{ErrorSeverity, IError, ParserError},
+        position::Position,
+        span::Span,
+    },
     frontend::{
-        ast::{Node, Statement},
+        ast::{Block, Expression, Literal, Node, Statement},
         lexer::lexer::ILexer,
         parser::{core::try_consume_token, Parser},
         tokens::TokenCategory,
     },
 };
 
+fn synthetic_break_block(span: Span) -> Node<Block> {
+    Node {
+        value: Block(vec![Node {
+            value: Statement::Break,
+            span: span.clone(),
+        }]),
+        span,
+    }
+}
+
 impl<L: ILexer> Parser<L> {
     pub(in crate::frontend::parser) fn parse_while_statement(&mut self) -> Result<Option<Node<Statement>>, Box<dyn IError>> {
-        // while_statement = "while", "(", expression, ")", statement_block;
+        // while_statement = "while", "(", expression, ")", statement_block
+        //                 | "while", "(", expression, [ "not" ], "matches", match_arm_without_block, ")", statement_block;
 
         let while_token = try_consume_token!(self, TokenCategory::While);
 
@@ -20,7 +35,9 @@ impl<L: ILexer> Parser<L> {
             .parse_expression()?
             .ok_or_else(|| self.create_parser_error(String::from("Couldn't create expression while parsing while statement.")))?;
 
-        let _ = self.consume_must_be(TokenCategory::ParenClose)?;
+        if self.consume_if_matches(TokenCategory::ParenClose)?.is_none() {
+            return self.finish_while_matches_statement(condition, while_token.span.start());
+        }
 
         let block = self
             .parse_statement_block()?
@@ -34,5 +51,68 @@ impl<L: ILexer> Parser<L> {
         };
 
         Ok(Some(node))
+    }
+
+    fn finish_while_matches_statement(
+        &mut self,
+        condition: Node<Expression>,
+        start_pos: Position,
+    ) -> Result<Option<Node<Statement>>, Box<dyn IError>> {
+        let is_negating = self.consume_if_matches(TokenCategory::Not)?.is_some();
+        let _ = self.consume_must_be(TokenCategory::Matches)?;
+
+        let mut arm = self
+            .parse_match_arm_without_block()?
+            .ok_or_else(|| self.create_parser_error(String::from("Couldn't create match arm while parsing while-matches statement.")))?;
+
+        let _ = self.consume_must_be(TokenCategory::ParenClose)?;
+
+        let body = self
+            .parse_statement_block()?
+            .ok_or_else(|| self.create_parser_error(String::from("Couldn't create statement block while parsing while-matches statement.")))?;
+
+        if is_negating {
+            if let Some(var_val) = &arm.value.variant_value {
+                return Err(Box::new(ParserError::at(
+                    ErrorSeverity::HIGH,
+                    String::from("Cannot take the inner value of enum variant in while-not-matches statement."),
+                    var_val.span.clone(),
+                )));
+            }
+        }
+
+        let match_span = Span::new(condition.span.start(), body.span.end());
+        let loop_span = Span::new(start_pos, body.span.end());
+        let break_block = synthetic_break_block(condition.span.clone());
+
+        let true_condition = Node {
+            value: Expression::Literal(Literal::True),
+            span: condition.span.clone(),
+        };
+
+        let (arm_block, rest_block) = if is_negating { (break_block, body) } else { (body, break_block) };
+        arm.value.block = arm_block;
+
+        let match_stmt = Node {
+            value: Statement::Match {
+                expression: condition,
+                match_arms: vec![arm],
+                rest_arm: Some(rest_block),
+            },
+            span: match_span.clone(),
+        };
+
+        let loop_block = Node {
+            value: Block(vec![match_stmt]),
+            span: match_span,
+        };
+
+        Ok(Some(Node {
+            value: Statement::WhileLoop {
+                condition: true_condition,
+                block: loop_block,
+            },
+            span: loop_span,
+        }))
     }
 }
